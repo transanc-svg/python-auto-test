44d828338500bc6f326a3f4c7e7471982caba024
transanc-svg <transanc@gmail.com>
2025-10-07 10:08:52 +0900

    Update finland.py

--- finland.py
+++ finland.py
@@ -21,7 +21,7 @@ creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 
 # --- スプレッドシート名 ---
-spreadsheet_name = "finland"
+spreadsheet_name = "フィンランド"
 sheet = client.open(spreadsheet_name).sheet1
 
 # --- 既存のURLを取得して重複防止 ---
@@ -90,6 +90,7 @@ for entry in entries_to_process:
 
 driver.quit()
 print("最新10件のニュースから og:image 付きの記事のみをスプレッドシート 'thai' に追加しました。")
+
 
 
 
